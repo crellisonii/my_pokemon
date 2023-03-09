7c79d0a647a8fb80408f74187e85fe8b933525f5
Charles Ellison <crelliso@gmail.com>
2023-03-08 21:50:22 -0600

    implement server.ts

--- server.ts
+++ server.ts
@@ -1,1 +1,28 @@
 import "reflect-metadata";
+
+import { ApolloServer } from "apollo-server-express";
+import { BerryResolver } from "./server/modules/berries/resolver";
+import { app } from "./server/app";
+import { buildSchema } from "type-graphql";
+
+const port = 4000;
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [BerryResolver],
+  });
+
+  const server = await new ApolloServer({
+    schema,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+  });
+}
+
+bootstrap();
