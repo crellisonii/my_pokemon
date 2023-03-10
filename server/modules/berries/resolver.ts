cc205ac70fb7a7329c1867dfaa7400c3355dd2be
Charles Ellison <crelliso@gmail.com>
2023-03-09 18:40:15 -0600

    add all queries
    * create getAllBerries query
    * create getAllBerryFirmness query
    * create getAllBerryFlavors query

--- server/modules/berries/resolver.ts
+++ server/modules/berries/resolver.ts
@@ -1,7 +1,10 @@
 import { Arg, Query, Resolver } from "type-graphql";
 import { baseUrl } from "../../constants";
 import { Berry, BerryFirmness, BerryFlavor } from "./types";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { NamedAPIResourceList, PaginationInput } from "../shared";
+import { ApiError } from "../../interfaces";
+import { getApiError } from "../../helpers";
 
 @Resolver()
 export class BerryResolver {
@@ -12,69 +15,119 @@ export class BerryResolver {
   @Query(returns => Berry)
   async getBerry(@Arg("nameId") input: string): Promise<Berry> {
     console.log(
-      `🚀 ~ file: resolver.ts:15 ~ BerryResolver ~ getBerry ~ input:`,
+      `🚀 ~ file: resolver.ts:18 ~ BerryResolver ~ getBerry ~ input:`,
       input
     );
     try {
       const url = `${baseUrl}${this.berryUrl}/${input}`;
-      const response = await axios.get(url);
       console.log(
-        `🚀 ~ file: resolver.ts:19 ~ BerryResolver ~ getBerry ~ response:`,
-        response
+        `🚀 ~ file: resolver.ts:24 ~ BerryResolver ~ getBerry ~ url:`,
+        url
       );
+      const response = await axios.get(url);
       return response.data;
     } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => NamedAPIResourceList)
+  async getAllBerries(
+    @Arg("pagination") input: PaginationInput
+  ): Promise<NamedAPIResourceList> {
+    console.log(`🚀 ~ file: resolver.ts:40 ~ BerryResolver ~ input:`, input);
+    try {
+      let url = `${baseUrl}${this.berryUrl}?limit=${input.limit}&offset=${input.offset}`;
       console.log(
-        `🚀 ~ file: resolver.ts:23 ~ BerryResolver ~ getBerry ~ e:`,
-        e
+        `🚀 ~ file: resolver.ts:44 ~ BerryResolver ~ getAllBerries ~ url:`,
+        url
       );
-      throw new Error(JSON.stringify(e));
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
     }
   }
 
   @Query(returns => BerryFirmness)
   async getBerryFirmness(@Arg("nameId") input: string): Promise<BerryFirmness> {
     console.log(
-      `🚀 ~ file: resolver.ts:37 ~ BerryResolver ~ getBerryFirmness ~ input:`,
+      `🚀 ~ file: resolver.ts:59 ~ BerryResolver ~ getBerryFirmness ~ input:`,
       input
     );
     try {
       const url = `${baseUrl}${this.firmnessUrl}/${input}`;
-      const response = await axios.get(url);
       console.log(
-        `🚀 ~ file: resolver.ts:41 ~ BerryResolver ~ getBerryFirmness ~ response:`,
-        response
+        `🚀 ~ file: resolver.ts:64 ~ BerryResolver ~ getBerryFirmness ~ url:`,
+        url
       );
+      const response = await axios.get(url);
       return response.data;
     } catch (e) {
-      console.log(
-        `🚀 ~ file: resolver.ts:45 ~ BerryResolver ~ getBerryFirmness ~ e:`,
-        e
-      );
-      throw new Error(JSON.stringify(e));
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => NamedAPIResourceList)
+  async getAllBerryFirmness(
+    @Arg("pagination") input: PaginationInput
+  ): Promise<NamedAPIResourceList> {
+    console.log(`🚀 ~ file: resolver.ts:78 ~ BerryResolver ~ input:`, input);
+    try {
+      let url = `${baseUrl}${this.firmnessUrl}?limit=${input.limit}&offset=${input.offset}`;
+      console.log(`🚀 ~ file: resolver.ts:81 ~ BerryResolver ~ url:`, url);
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
     }
   }
 
   @Query(returns => BerryFlavor)
   async getBerryFlavor(@Arg("nameId") input: string): Promise<BerryFlavor> {
     console.log(
-      `🚀 ~ file: resolver.ts:52 ~ BerryResolver ~ getBerryFlavor ~ input:`,
+      `🚀 ~ file: resolver.ts:94 ~ BerryResolver ~ getBerryFlavor ~ input:`,
       input
     );
     try {
       const url = `${baseUrl}${this.flavorUrl}/${input}`;
-      const response = await axios.get(url);
       console.log(
-        `🚀 ~ file: resolver.ts:56 ~ BerryResolver ~ getBerryFlavor ~ response:`,
-        response
+        `🚀 ~ file: resolver.ts:99 ~ BerryResolver ~ getBerryFlavor ~ url:`,
+        url
       );
+      const response = await axios.get(url);
       return response.data;
     } catch (e) {
-      console.log(
-        `🚀 ~ file: resolver.ts:60 ~ BerryResolver ~ getBerryFlavor ~ e:`,
-        e
-      );
-      throw new Error(JSON.stringify(e));
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => NamedAPIResourceList)
+  async getAllBerryFlavors(
+    @Arg("pagination") input: PaginationInput
+  ): Promise<NamedAPIResourceList> {
+    console.log(
+      `🚀 ~ file: resolver.ts:117 ~ BerryResolver ~ getAllBerryFlavors ~ input:`,
+      input
+    );
+    try {
+      const url = `${baseUrl}${this.flavorUrl}?limit=${input.limit}&${input.offset}`;
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
     }
   }
 }
