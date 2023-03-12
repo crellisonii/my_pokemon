38cd2692a91a6bd7ad7686691d3e60d0103b67ba
Charles Ellison <crelliso@gmail.com>
2023-03-12 03:13:09 -0500

    make objects nullable

--- server/modules/shared/shared-types.ts
+++ server/modules/shared/shared-types.ts
@@ -9,55 +9,51 @@ import {
 } from "../named-api-resources";
 import { Field, Int, ObjectType } from "type-graphql";
 
-@ObjectType()
-export class APIResource {
-  @Field(type => String)
-  url: string;
-}
+import { APIResource } from "../api-resources";
 
 @ObjectType()
 export class Description {
-  @Field(type => String)
-  description: string;
+  @Field(type => String, { nullable: true })
+  description: string | null;
 
-  @Field(type => LanguageResource)
-  language: LanguageResource;
+  @Field(type => LanguageResource, { nullable: true })
+  language: LanguageResource | null;
 }
 
 @ObjectType()
 export class Effect {
-  @Field(type => String)
-  effect: string;
+  @Field(type => String, { nullable: true })
+  effect: string | null;
 
-  @Field(type => LanguageResource)
-  language: LanguageResource;
+  @Field(type => LanguageResource, { nullable: true })
+  language: LanguageResource | null;
 }
 
 @ObjectType()
 export class Encounter {
-  @Field(type => Int)
-  chance: number;
+  @Field(type => Int, { nullable: true })
+  chance: number | null;
 
-  @Field(type => EncounterConditionValueResource)
-  condition_values: EncounterConditionValueResource;
+  @Field(type => EncounterConditionValueResource, { nullable: true })
+  condition_values: EncounterConditionValueResource | null;
 
-  @Field(type => Int)
-  max_level: number;
+  @Field(type => Int, { nullable: true })
+  max_level: number | null;
 
-  @Field(type => EncounterMethodResource)
-  method: EncounterMethodResource;
+  @Field(type => EncounterMethodResource, { nullable: true })
+  method: EncounterMethodResource | null;
 
-  @Field(type => Int)
-  min_level: number;
+  @Field(type => Int, { nullable: true })
+  min_level: number | null;
 }
 
 @ObjectType()
 export class FlavorText {
-  @Field(type => String)
-  flavor_text: string;
+  @Field(type => String, { nullable: true })
+  flavor_text: string | null;
 
-  @Field(type => LanguageResource)
-  language: LanguageResource;
+  @Field(type => LanguageResource, { nullable: true })
+  language: LanguageResource | null;
 
   @Field(type => VersionResource, { nullable: true })
   version: VersionResource | null;
@@ -65,35 +61,35 @@ export class FlavorText {
 
 @ObjectType()
 export class GenerationGameIndex {
-  @Field(type => Int)
-  game_index: number;
+  @Field(type => Int, { nullable: true })
+  game_index: number | null;
 
-  @Field(type => GenerationResource)
-  generation: GenerationResource;
+  @Field(type => GenerationResource, { nullable: true })
+  generation: GenerationResource | null;
 }
 
 @ObjectType()
 export class MachineVersionDetail {
-  @Field(type => APIResource)
-  machine: APIResource;
+  @Field(type => APIResource, { nullable: true })
+  machine: APIResource | null;
 
-  @Field(type => VersionGroupResource)
-  version_group: VersionGroupResource;
+  @Field(type => VersionGroupResource, { nullable: true })
+  version_group: VersionGroupResource | null;
 }
 
 @ObjectType()
 export class Name {
-  @Field(type => LanguageResource)
-  language: LanguageResource;
+  @Field(type => LanguageResource, { nullable: true })
+  language: LanguageResource | null;
 
-  @Field(type => String)
-  name: string;
+  @Field(type => String, { nullable: true })
+  name: string | null;
 }
 
 @ObjectType()
 export class NamedAPIResourceList {
-  @Field(type => Int)
-  count: number;
+  @Field(type => Int, { nullable: true })
+  count: number | null;
 
   @Field(type => String, { nullable: true })
   next: string | null;
@@ -101,51 +97,51 @@ export class NamedAPIResourceList {
   @Field(type => String, { nullable: true })
   previous: string | null;
 
-  @Field(type => [NamedAPIResource])
-  results: [NamedAPIResource];
+  @Field(type => [NamedAPIResource], { nullable: true })
+  results: [NamedAPIResource] | null;
 }
 
 @ObjectType()
 export class VerboseEffect {
-  @Field(type => String)
-  effect: string;
+  @Field(type => String, { nullable: true })
+  effect: string | null;
 
-  @Field(type => LanguageResource)
-  language: LanguageResource;
+  @Field(type => LanguageResource, { nullable: true })
+  language: LanguageResource | null;
 
-  @Field(type => String)
-  short_effect: string;
+  @Field(type => String, { nullable: true })
+  short_effect: string | null;
 }
 
 @ObjectType()
 export class VersionEncounterDetail {
-  @Field(type => [Encounter])
-  encounter_details: [Encounter];
+  @Field(type => [Encounter], { nullable: true })
+  encounter_details: [Encounter] | null;
 
-  @Field(type => Int)
-  max_chance: number;
+  @Field(type => Int, { nullable: true })
+  max_chance: number | null;
 
-  @Field(type => VersionResource)
-  version: VersionResource;
+  @Field(type => VersionResource, { nullable: true })
+  version: VersionResource | null;
 }
 
 @ObjectType()
 export class VersionGameIndex {
-  @Field(type => Int)
-  game_index: number;
+  @Field(type => Int, { nullable: true })
+  game_index: number | null;
 
-  @Field(type => VersionResource)
-  version: VersionResource;
+  @Field(type => VersionResource, { nullable: true })
+  version: VersionResource | null;
 }
 
 @ObjectType()
 export class VersionGroupFlavorText {
-  @Field(type => LanguageResource)
-  language: LanguageResource;
+  @Field(type => LanguageResource, { nullable: true })
+  language: LanguageResource | null;
 
-  @Field(type => String)
-  text: string;
+  @Field(type => String, { nullable: true })
+  text: string | null;
 
-  @Field(type => VersionGroupResource)
-  version_group: VersionGroupResource;
+  @Field(type => VersionGroupResource, { nullable: true })
+  version_group: VersionGroupResource | null;
 }
