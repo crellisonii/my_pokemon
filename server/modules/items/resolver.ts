4f52d9676be2f1e27bb5c897d9b240ad1a9cf999
Charles Ellison <crelliso@gmail.com>
2023-03-13 01:56:39 -0500

    create item resolver

---
+++ server/modules/items/resolver.ts
@@ -0,0 +1,258 @@
+import { Arg, Query, Resolver } from "type-graphql";
+import {
+  Item,
+  ItemAttribute,
+  ItemCategory,
+  ItemFlingEffect,
+  ItemPocket,
+} from "./types";
+import { baseUrl } from "../../constants/urls";
+import axios from "axios";
+import { getApiError } from "../../helpers";
+import { NamedAPIResourceList, PaginationInput } from "../shared";
+
+@Resolver()
+export class ItemResolver {
+  itemUrl = "item/";
+  itemAttributeUrl = "item-attribute/";
+  itemCategoryUrl = "item-category/";
+  itemFlingEffectUrl = "item-fling-effect/";
+  itemPocketUrl = "item-pocket/";
+
+  @Query(returns => Item)
+  async getItem(@Arg("nameId") input: string): Promise<Item> {
+    console.log(
+      `🚀 ~ file: resolver.ts:24 ~ ItemResolver ~ getItem ~ input:`,
+      input
+    );
+
+    try {
+      const url = `${baseUrl}${this.itemUrl}${input}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:31 ~ ItemResolver ~ getItem ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => NamedAPIResourceList)
+  async getAllItems(
+    @Arg("pagination") input: PaginationInput
+  ): Promise<NamedAPIResourceList> {
+    console.log(
+      `🚀 ~ file: resolver.ts:48 ~ ItemResolver ~ getAllItems ~ input:`,
+      input
+    );
+
+    try {
+      const { limit, offset } = input;
+      const url = `${baseUrl}${this.itemUrl}?limit=${limit}&offset=${offset}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:56 ~ ItemResolver ~ getAllItems ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => ItemAttribute)
+  async getItemAttribute(@Arg("nameId") input: string): Promise<ItemAttribute> {
+    console.log(
+      `🚀 ~ file: resolver.ts:71 ~ ItemResolver ~ getItemAttribute ~ input:`,
+      input
+    );
+
+    try {
+      const url = `${baseUrl}${this.itemUrl}${input}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:78 ~ ItemResolver ~ getItemAttribute ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => NamedAPIResourceList)
+  async getAllItemAttributes(
+    @Arg("pagination") input: PaginationInput
+  ): Promise<NamedAPIResourceList> {
+    console.log(
+      `🚀 ~ file: resolver.ts:95 ~ ItemResolver ~ getAllItemAttributes ~ input:`,
+      input
+    );
+
+    try {
+      const { limit, offset } = input;
+      const url = `${baseUrl}${this.itemUrl}?limit=${limit}&offset=${offset}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:103 ~ ItemResolver ~ getAllItemAttributes ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => ItemCategory)
+  async getItemCategory(@Arg("nameId") input: string): Promise<ItemCategory> {
+    console.log(
+      `🚀 ~ file: resolver.ts:118 ~ ItemResolver ~ getItemCategory ~ input:`,
+      input
+    );
+
+    try {
+      const url = `${baseUrl}${this.itemUrl}${input}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:125 ~ ItemResolver ~ getItemCategory ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => NamedAPIResourceList)
+  async getAllItemCategories(
+    @Arg("pagination") input: PaginationInput
+  ): Promise<NamedAPIResourceList> {
+    console.log(
+      `🚀 ~ file: resolver.ts:142 ~ ItemResolver ~ getAllItemCategories ~ input:`,
+      input
+    );
+
+    try {
+      const { limit, offset } = input;
+      const url = `${baseUrl}${this.itemUrl}?limit=${limit}&offset=${offset}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:150 ~ ItemResolver ~ getAllItemCategories ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => ItemFlingEffect)
+  async getItemFlingEffect(
+    @Arg("nameId") input: string
+  ): Promise<ItemFlingEffect> {
+    console.log(
+      `🚀 ~ file: resolver.ts:167 ~ ItemResolver ~ getItemFlingEffect ~ input:`,
+      input
+    );
+
+    try {
+      const url = `${baseUrl}${this.itemUrl}${input}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:174 ~ ItemResolver ~ getItemFlingEffect ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => NamedAPIResourceList)
+  async getAllItemFlingEffects(
+    @Arg("pagination") input: PaginationInput
+  ): Promise<NamedAPIResourceList> {
+    console.log(
+      `🚀 ~ file: resolver.ts:191 ~ ItemResolver ~ getAllItemFlingEffects ~ input:`,
+      input
+    );
+
+    try {
+      const { limit, offset } = input;
+      const url = `${baseUrl}${this.itemUrl}?limit=${limit}&offset=${offset}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:199 ~ ItemResolver ~ getAllItemFlingEffects ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => ItemPocket)
+  async getItemPocket(@Arg("nameId") input: string): Promise<ItemPocket> {
+    console.log(
+      `🚀 ~ file: resolver.ts:214 ~ ItemResolver ~ getItemPocket ~ input:`,
+      input
+    );
+
+    try {
+      const url = `${baseUrl}${this.itemUrl}${input}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:221 ~ ItemResolver ~ getItemPocket ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  @Query(returns => NamedAPIResourceList)
+  async getAllItemPockets(
+    @Arg("pagination") input: PaginationInput
+  ): Promise<NamedAPIResourceList> {
+    console.log(
+      `🚀 ~ file: resolver.ts:238 ~ ItemResolver ~ getAllItemPockets ~ input:`,
+      input
+    );
+
+    try {
+      const { limit, offset } = input;
+      const url = `${baseUrl}${this.itemUrl}?limit=${limit}&offset=${offset}`;
+      console.log(
+        `🚀 ~ file: resolver.ts:246 ~ ItemResolver ~ getAllItemPockets ~ url:`,
+        url
+      );
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      const error = getApiError(e);
+      console.table(error);
+      throw new Error(JSON.stringify(error));
+    }
+  }
+}
